ee3e51a466cbb573b90c689fdcdcc54ac0bfdab1
carlos-sardina <developerAlbertoMT@gmail.com>
2023-11-25 20:43:50 -0600

    login w .env values

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const getTasksFromTrello = require("./src/getTasksFromTrello");
+const loginIntoTodoist = require("./src/loginIntoTodoist");
 
 async function main() {
   const tasks = await getTasksFromTrello();
-  console.log('Trello tasks:', tasks);
+  const { browser, page } = await loginIntoTodoist(process.env.TODOIST_EMAIL, process.env.TODOIST_PASSWORD);
 }
 
 main();
---
+++ src/loginIntoTodoist.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+
+/**
+ * Login Todoist.
+ * @param {string} email - Todoist email.
+ * @param {string} password - Todoist password.
+ * @returns {Promise<{browser: import('puppeteer').Browser, page: import('puppeteer').Page}>}
+ */
+async function loginIntoTodoist(email, password) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  try {
+    await page.goto('https://app.todoist.com/auth/login');
+
+    await page.waitForSelector('input[type="email"]');
+    await page.type('input[type="email"]', email);
+
+    await page.waitForSelector('input[type="password"]');
+    await page.type('input[type="password"]', password);
+
+    await page.waitForSelector('button[type="submit"]');
+    await page.click('button[type="submit"]');
+    await page.waitForNavigation();
+
+    return { browser, page };
+  } catch (e) {
+    console.error('Error during login:', e);
+    await browser.close();
+  }
+}
+
+module.exports = loginIntoTodoist;
